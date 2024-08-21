f3f9ff494e81589377b284e9b4867847bb436877
Emil <e.roydev@gmail.com>
2024-08-22 00:04:18 +0300

    authentication half complete

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,15 +1,39 @@
+from django.contrib.auth import get_user_model
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from .serializers import UserSerializer
+from rest_framework import status
+from rest_framework.authtoken.models import Token
 
+from django.shortcuts import get_object_or_404
+
+
+UserModel = get_user_model()
 @api_view(['POST'])
 def login(request):
-    return Response({})
+    user = get_object_or_404(UserModel, email=request.data['email'])
+
+    if not user.check_password(request.data['password']):
+        return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+
+    token, created = Token.objects.get_or_create(user=user)
+    serializer = UserSerializer(instance=user)
+    return Response({"token": token.key, "user": serializer.data})
 
 @api_view(['POST'])
 def signup(request):
-    return Response({})
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        user = UserModel.objects.get(email=request.data['email'])
+        user.set_password(request.data['password'])
+        user.save()
+        token = Token.objects.create(user=user)
+        return Response({"token": token.key, "user": serializer.data})
 
-@api_view(['POST'])
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
 def test_token(request):
     return Response({})
