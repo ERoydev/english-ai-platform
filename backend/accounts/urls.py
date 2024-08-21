e3e310362a0175d229cc3e59f4543f9edf27fb0f
Emil <e.roydev@gmail.com>
2024-08-22 00:04:18 +0300

    authentication half complete

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    re_path('signup/', views.signup.as_view(), name='signup'),
-    re_path('login/', views.login.as_view(), name='login'),
+    re_path('login', views.login, name='login'),
+    re_path('signup', views.signup, name='signup'),
     re_path('test_token', views.test_token, name='test_token'),
 ]
