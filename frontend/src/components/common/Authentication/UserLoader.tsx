9c3a596991fc6fc94c411fb3c2cf1addcb4cffa2
Emil <e.roydev@gmail.com>
2024-09-28 04:06:54 +0300

    presisting state
    I have created userLoader component which implement the useEffect() logic to load user by token and then this component wraps my entire application to ensure that for every reload i will take userData if logged-in

---
+++ frontend/src/components/common/Authentication/UserLoader.tsx
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux"
+import { loadUserFromToken } from "../../../store/Auth/authActions";
+
+/*
+    I use this component to wrap all my application to make sure i will load the userdata(if logged in) everytime i refresh the page.
+*/
+
+
+
+const UserLoader = ({ children }) => {
+    const dispatch = useDispatch();
+    const userToken = useSelector(state => state.auth.userToken);
+
+  
+    useEffect(() => {
+      if (userToken) {
+        // Dispatching the async thunk to load user data
+        dispatch(loadUserFromToken({ token: userToken }));
+      }
+    }, [dispatch, userToken]);
+  
+    return children; // Render all child components, which would include my routes
+  };
+
+
+export default UserLoader;
