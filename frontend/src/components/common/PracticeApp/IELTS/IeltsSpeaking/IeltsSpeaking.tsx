3e55e1ee86829732c4ff73f058030078ba925d82
Emil <e.roydev@gmail.com>
2024-10-10 12:21:26 +0300

    Move Recording logic in different component to make it reusable

--- frontend/src/components/common/PracticeApp/IELTS/IeltsSpeaking/IeltsSpeaking.tsx
+++ frontend/src/components/common/PracticeApp/IELTS/IeltsSpeaking/IeltsSpeaking.tsx
@@ -1,63 +1,13 @@
 import { useState } from "react";
 import chapter1 from "./questions";
 import Navigation from "../../../Navigation/Navigation";
-import SpeakButton from "../../../shared/Button/speakButton";
-import TimerComponent from "../../../shared/Timer/TimerComponent";
+import RecordingComponent from "../../../../functionalComponents/RecordingComponent/RecordingComponent";
+import { useRecordingChangeQuestions } from "../../../../functionalComponents/RecordingComponent/useRecordingChangeQuestions";
 
 
 export default function IeltsSpeaking() {
+    const { currentChapterIndex, currentQuestionIndex, nextButtonHandler} = useRecordingChangeQuestions(chapter1)
     
-    const [currentChapterIndex, setCurrentChapterIndex] = useState(0);
-    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-    const [recordButtonText, setRecordButtonText] = useState('Start Record');
-    const [pauseTimer, setPauseTimer] = useState(false);
-
-    const recordButtonHandler = () => {
-
-        if (recordButtonText === "Pause") {
-            setRecordButtonText(() => {
-                return "Continue Recording";
-            })
-
-            handleStartPauseClick();
-
-        } else {
-            setRecordButtonText(() => {
-                return "Pause";
-            })
-
-            handleStartPauseClick();
-
-        }
-    }
-
-    const nextButtonHandler = () => {
-        const currChapterLength = chapter1[currentChapterIndex].questions.length - 1
-        const countOfTotalChapters = chapter1.length - 1;
-        
-        if (currentChapterIndex == countOfTotalChapters && currentQuestionIndex == currChapterLength ) {
-            return
-        }
-       
-        if (currentQuestionIndex < currChapterLength) {
-            setCurrentQuestionIndex((idx) => {
-                return idx + 1
-            })
-        } else {
-            setCurrentChapterIndex((idx) => {
-                return idx + 1
-            })
-
-            setCurrentQuestionIndex(() => {
-                return 0
-            })
-        }
-    }
-
-    const handleStartPauseClick = () => {
-        setPauseTimer((prev) => !prev); // Toggle between start and pause
-    };
-
 
     return(
         <section className="h-screen flex flex-col relative max-container">
@@ -80,31 +30,8 @@ export default function IeltsSpeaking() {
                         <p className="text-sm text-slate-400">The longer you talk the better</p>
                     </div>
 
-                    {/* BUTTONS */}
-                    <div className="flex justify-between items-center absolute bottom-10 left-0 right-0 px-5">
-                        
-                        <div className="w-1/3 flex justify-start items-center">
-                            <SpeakButton buttonText={recordButtonText} buttonHandler={recordButtonHandler} />
-                        </div>
-
-                        <div className="w-1/3 flex justify-center items-center">
-                            <TimerComponent pauseTimer={pauseTimer} />
-                        </div>
-
-                        <div className="w-1/3 flex justify-end items-center">
-                            <button
-                                onClick={nextButtonHandler}
-                                className="bg-[#333] rounded-md text-white p-4 max-md:text-sm max-md:p-2 hover:cursor-pointer hover:bg-gray-600">
-                                <svg
-                                    className="w-[30px] fill-white"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 512 512"
-                                >
-                                    <path d="M470.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 256 265.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160zm-352 160l160-160c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L210.7 256 73.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0z"/>
-                                </svg>
-                            </button>
-                        </div>
-                    </div>
+                    {/* Recording Component with timer and FN from my custom hook is used here  */}
+                    <RecordingComponent nextButtonHandler={nextButtonHandler}/>
                 </div>
             </div>
         </section>
