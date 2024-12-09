84ba4c27295ade48a1432db6f491f7342e390c70
Emil Roydev <e.roydev@gmail.com>
2024-12-09 14:24:56 +0200

    simple button handlers added on frontend

--- frontend/src/components/common/ProfilePage/components/AccountSettings.tsx
+++ frontend/src/components/common/ProfilePage/components/AccountSettings.tsx
@@ -13,6 +13,7 @@ import { logout } from "../../../../store/Auth/authSlice";
 export default function AccountSettings() {
     const userData = useSelector(state => state.auth.userInfo)
     const [isModalOpen, setIsModalOpen] = useState(false); // State for modal visibility
+    const [passwordInputField, setPasswordInputField] = useState('password');
     const navigate = useNavigate();
 
     const dispatch: AppDispatch = useDispatch();
@@ -36,6 +37,14 @@ export default function AccountSettings() {
     const handleCancelDelete = () => {
         setIsModalOpen(false); // Close the modal without deleting
     };
+
+    const showHiddenPasswordClickHandler = () => {
+        if (passwordInputField === 'password') {
+            setPasswordInputField('text');
+        } else {
+            setPasswordInputField('password');
+        }
+    }
 
     return(
         <BaseProfile>
@@ -65,24 +74,23 @@ export default function AccountSettings() {
                                 <label htmlFor="current-password">
                                     <span className="text-sm text-gray-500">Current Password</span>
                                     <div className="relative flex overflow-hidden rounded-md border-2 transition focus-within:border-blue-600">
-                                        <input type="password" id="current-password" className="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none" placeholder="***********" />
+                                        <input type={passwordInputField} id="current-password" className="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none" placeholder="***********" />
                                     </div>
                                 </label>
 
                                 <label htmlFor="new-password">
                                     <span className="text-sm text-gray-500">New Password</span>
                                     <div className="relative flex overflow-hidden rounded-md border-2 transition focus-within:border-blue-600">
-                                        <input type="password" id="new-password" className="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none" placeholder="***********" />
+                                        <input type={passwordInputField} id="new-password" className="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none" placeholder="***********" />
                                     </div>
                                 </label>
                             </div>
 
-                            <svg xmlns="http://www.w3.org/2000/svg" className="mt-5 ml-2 h-6 w-6 cursor-pointer text-sm font-semibold text-gray-600 underline decoration-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                            <svg onClick={showHiddenPasswordClickHandler} xmlns="http://www.w3.org/2000/svg" className="mt-5 ml-2 h-6 w-6 cursor-pointer text-sm font-semibold text-gray-600 underline decoration-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21" />
                             </svg>
                         </div>
 
-                        <p className="mt-2">Can't remember your current password. <a className="text-sm font-semibold text-blue-600 underline decoration-2" href="#">Recover Account</a></p>
                         <button className="mt-4 rounded-lg bg-blue-600 px-4 py-2 text-white">Save Password</button>
                         <hr className="mt-4 mb-8" />
                     </div>
